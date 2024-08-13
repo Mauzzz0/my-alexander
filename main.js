45f40e25e999be0473387ecfffa47751b7d92958
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-13 18:35:49 +0300

    Добавлен новый код

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log(0);
+const names = ['a', 'b', 'c'];
+
+const james = names;
+
+james.push('d')
+
+console.log(james);
+console.log(names);
+console.log(names === names); // true
+
+const ar1 = ['a'];
+const ar2 = ['a'];
+console.log(ar1 === ar2); // false
