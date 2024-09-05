895f1ef8f44e80ffcc1d008440c72d0109238570
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-09-05 18:45:19 +0300

    Билдер функций

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,33 @@
-type ExampleType2 = (x: string) => void;
-const logInput: ExampleType2 = (input: string) => console.log(input);
-logInput('ajshdakjsd');
-
-type ExampleType3 = (a: string, b: number) => void;
-
-const func3: ExampleType3 = (name: string, age: number) => console.log(name, age);
-func3('ruslan', 10);
-
-type ExampleType4 = (a: number, b: number) => number;
-const sum: ExampleType4 = (a: number, b: number) => {
+const sumTwoNumbers = (a: number, b: number) => {
   return a + b;
 };
 
-const result = sum(4, 5);
-console.log(result);
+const sum1And1 = sumTwoNumbers(1, 1);
+const sum1And2 = sumTwoNumbers(1, 2);
+const sum1And3 = sumTwoNumbers(1, 3);
+const sum1And4 = sumTwoNumbers(1, 4);
 
-const sum3: (arg: number, brg: number, crg: number) => number = (a: number, b: number, c: number) =>
-  a + b + c;
+const sum2And1 = sumTwoNumbers(2, 1);
+const sum2And2 = sumTwoNumbers(2, 2);
+const sum2And3 = sumTwoNumbers(2, 3);
+const sum2And4 = sumTwoNumbers(2, 4);
 
-console.log(sum3(1, 2, 3));
+const sumWithNumber = (a: number) => {
+  return (b: number) => {
+    return a + b;
+  };
+};
+
+const sumWithNumber_ = (a: number) => (b: number) => a + b;
+
+const sumWith1 = sumWithNumber(1); // (b: number) => return 1 + b;
+const sum1And1_ = sumWith1(1);
+const sum1And2_ = sumWith1(2);
+const sum1And3_ = sumWith1(3);
+const sum1And4_ = sumWith1(4);
+
+const sumWith2 = sumWithNumber(2); // (b: number) => return 2 + b;
+const sum2And1_ = sumWith2(1);
+const sum2And2_ = sumWith2(2);
+const sum2And3_ = sumWith2(3);
+const sum2And4_ = sumWith2(4);
