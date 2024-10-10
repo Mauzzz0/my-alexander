49039826dba84ab8bcad285ddc5c6c9883302f0f
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-10-10 18:20:46 +0300

    Кодирование

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
-const factorial = (num: number): number => {
-  if (num === 1) {
-    return 1;
+const original = 'the quick brown fox jumps over the lazy dog';
+const encoded = 'sgd pthbj aqnvm enw itlor nudq sgd kzyx cnf';
+
+const dict: Record<string, string> = {};
+
+for (let i = 0; i < original.length; i++) {
+  const originalKey = original[i];
+  const encodedKey = encoded[i];
+
+  dict[encodedKey] = originalKey;
+}
+
+console.log(dict);
+
+const myText = 'xnt gzud lzjhmf fqdzs oqnfqdrr hm kdzqmhmf izuzrbqhos. bnmfqzsr!';
+let result = '';
+
+for (const letter of myText) {
+  const encoded = dict[letter];
+
+  if (encoded) {
+    result += encoded;
+  } else {
+    result += letter;
   }
+}
 
-  const prevFactorial = factorial(num - 1);
-
-  return num * prevFactorial;
-};
-
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(10));
+console.log(result);
